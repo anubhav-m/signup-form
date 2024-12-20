7befa38f9d7d422d752acbe779df0bb2c15c3d5f
Anubhav Mishra <mishra.anubhav108@gmail.com>
2024-12-20 21:08:45 +0530

    intialised project with bg and main container
        -theme toggling support added
        -bg created
        -main container created

---
+++ scripts.js
@@ -0,0 +1,27 @@
+// Set the initial theme and toggle button visibility
+const root = document.documentElement;
+root.className = 'dark';
+
+const lightToggle = document.querySelector(".light-toggle");
+const darkToggle = document.querySelector(".dark-toggle");
+
+lightToggle.style.display = "none"; // Start with light toggle hidden
+
+function setTheme() {
+    const currentTheme = root.className;
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+
+    // Toggle the visibility of buttons based on the new theme
+    if (newTheme === 'light') {
+        lightToggle.style.display = "block";
+        darkToggle.style.display = "none";
+    } else {
+        lightToggle.style.display = "none";
+        darkToggle.style.display = "block";
+    }
+}
+
+// Add a click event listener to both toggle buttons
+darkToggle.addEventListener('click', setTheme);
+lightToggle.addEventListener('click', setTheme);
